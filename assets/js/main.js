8aa6ecd77dfb80514459451781b7ec50373ba326
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-02-12 15:01:13 +0100

    added Number.parseFloat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,38 @@
 let bill = document.getElementById('bill')
 let people = document.getElementById('people')
 let service = document.getElementById('service')
-let output = document.getElementById('output')
-let noEntry = document.getElementById('noEntry')
 
-let trinkgeld = document.getElementById('trinkgeld')
-let gesamtBetrag = document.getElementById('gesamtBetrag')
-let preisProPerson = document.getElementById('preisProPerson')
+let output = document.getElementById('output')
+
+
 
 calc = () =>{
     
+
     switch(true){
 
+
         case service.value == "1":
-            output.innerHTML = "<h2>Preis Pro Person beträgt: " + bill.value / people.value + "€" + "<br>" +
-            "Das Trinkgeld beträgt: " + bill.value / 100 * 1.5 + "€" + "<br>" +
-            "Die Gesamtsumme beträgt: " + ((bill.value / people.value) + (bill.value/100*1.5)) + "€</h2>"
+            output.innerHTML = "<h2>Das Trinkgeld beträgt: " + Number.parseFloat(bill.value / 100 * 2).toFixed(2) + "€" + "<br>" 
+            +
+            "Die Gesamtsumme beträgt: " + Number.parseFloat(bill.value * 1 + (bill.value / 100 * 2)).toFixed(2) + "€"
+            +
+            "<h2>Preis Pro Person beträgt: " + Number.parseFloat(bill.value / people.value + (bill.value / 100 * 2)).toFixed(2) + "€" + "<br></h2>"
             break;
-
+    
         case service.value == "2":
-            output.innerHTML = "<h2>Preis Pro Person beträgt: " + bill.value / people.value + "€" + "<br>" +
-            "Das Trinkgeld beträgt: " + bill.value / 100 * 2 + "€" + "<br>" +
-            "Die Gesamtsumme beträgt: " + ((bill.value / people.value) + (bill.value/100*2)) + "€</h2>"
+            output.innerHTML = "<h2>Das Trinkgeld beträgt: " + Number.parseFloat(bill.value / 100 * 10).toFixed(2) + "€" + "<br>" 
+            +
+            "Die Gesamtsumme beträgt: " + Number.parseFloat(bill.value * 1 + (bill.value / 100 * 10)).toFixed(2) + "€"
+            +
+            "<h2>Preis Pro Person beträgt: " + Number.parseFloat(bill.value / people.value + (bill.value / 100 * 10)).toFixed(2) + "€" + "<br></h2>"
             break;
-
         case service.value == "3":
-            output.innerHTML = "<h2>Preis Pro Person beträgt: " + bill.value / people.value + "€" + "<br>" +
-            "Das Trinkgeld beträgt: " + bill.value / 100 * 2.5 + "€"
-            + "<br>" + "Die Gesamtsumme beträgt: " + ((bill.value / people.value) + (bill.value/100*2.5)) + "€</h2>"
+            output.innerHTML = "<h2>Das Trinkgeld beträgt: " + Number.parseFloat(bill.value / 100 * 20).toFixed(2) + "€" + "<br>" 
+            +
+            "Die Gesamtsumme beträgt: " + Number.parseFloat(bill.value * 1 + (bill.value / 100 * 20)).toFixed(2) + "€"
+            +
+            "<h2>Preis Pro Person beträgt: " + Number.parseFloat(bill.value / people.value + (bill.value / 100 * 20)).toFixed(2) + "€" + "<br></h2>"
             break;
 
         default:
